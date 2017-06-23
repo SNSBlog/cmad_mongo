21cbc531a2b0a5257183e9d7cf28635d26956787
shmaity <shmaity@cisco.com>
2017-06-23 10:25:53 +0530

    First commit

---
+++ src/main/java/com/nds/mongo/data/MongoConnection.java
@@ -0,0 +1,12 @@
+package com.nds.mongo.data;
+
+public class MongoConnection {
+	
+	//10.197.24.69
+	
+	//104.198.37.172
+	
+	public static String mongoServerIP = "104.198.37.172";
+	public static int mongoServerPort = 27017;
+	
+}
---
+++ src/main/java/com/nds/mongo/data/UserDAOInterface.java
@@ -0,0 +1,17 @@
+package com.nds.mongo.data;
+
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.nds.mongo.api.User;
+
+public interface UserDAOInterface {
+
+	public void createUser(User user);
+
+	public void removeuser(String uname);
+
+	public List<BasicDBObject> checkuser(String login, String passwd);
+
+	public List<BasicDBObject> searchuserpost(String user1);
+}
